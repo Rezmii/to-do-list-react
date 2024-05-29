d77b707c47fcf2ffd6f9fbd89207cbf8572077bd
Rezmii <bartoszrezmer20@gmail.com>
2024-05-29 12:41:20 +0200

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { Button } from "@chakra-ui/react";
+import { Box, HStack, Text } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <Button>test</Button>;
+  return (
+    <Box height="90vh" width="90vw" borderRadius="25px" bg="lightgray">
+      <HStack margin={5}>
+        <Text fontSize="3xl">Welcome</Text>
+      </HStack>
+    </Box>
+  );
 }
 
 export default App;
