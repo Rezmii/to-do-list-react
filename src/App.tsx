0b5c9b4dd6b593bdd6d6ac3b48fa196d3f1286f3
Rezmii <bartoszrezmer20@gmail.com>
2024-05-29 12:42:28 +0200

    Build seperate Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { Box, HStack, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Box height="90vh" width="90vw" borderRadius="25px" bg="lightgray">
-      <HStack margin={5}>
-        <Text fontSize="3xl">Welcome</Text>
-      </HStack>
+      <Header />
     </Box>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { HStack, Text } from "@chakra-ui/react";
+
+const Header = () => {
+  return (
+    <HStack margin={5}>
+      <Text fontSize="3xl">Welcome</Text>
+    </HStack>
+  );
+};
+
+export default Header;
