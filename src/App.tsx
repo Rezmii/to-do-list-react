ca219bdbb2feadbf5fcf0ca071c2582f49f727e8
Rezmii <bartoszrezmer20@gmail.com>
2024-05-29 12:10:28 +0200

    setting up chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Button } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>test</div>
-    </>
-  );
+  return <Button>test</Button>;
 }
 
 export default App;
